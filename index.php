a7e96ece7a4c2f0f156df45088cfa39f84db9c1a
cotucotam <trancotam@gmail.com>
2025-02-05 22:06:02 +0700

    no 3
    Signed-off-by: cotucotam <trancotam@gmail.com>

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<?php 
-    $name = array('tran', 'co', 'tam');
-    $count = 0;
-    while($count < count($name)){
-        echo "<li>Hello $name[$count]</li>";
-        $count ++;
-    }
-?>
+
+<?php
+// Tạo WP_Query để lấy bài viết theo ngày giảm dần
+$args = array(
+    'post_type'      => 'post',      // Lấy bài viết (post type là 'post')
+    'posts_per_page' => -1,          // Hiển thị tất cả bài viết
+    'orderby'        => 'date',      // Sắp xếp theo ngày
+    'order'          => 'DESC',      // Theo thứ tự giảm dần
+);
+$query = new WP_Query($args);
+
+if ($query->have_posts()){
+    while ($query->have_posts()) {
+     $query->the_post(); ?>
+        <h2>
+            <?php the_title(); ?>
+        </h2>
+        <div>
+            <span>By <?php the_author(); ?></span>
+            <span><?php the_date(); ?></span>
+        </div>
+        <div>
+            <?php the_excerpt(); // Hiển thị tóm tắt nội dung ?>
+        </div>
+
+    <?php }
+} ?>
+
+
