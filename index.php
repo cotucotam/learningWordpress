e35920c4ffa4ef6354ebbda63af40351e9b3ef8e
cotucotam <trancotam@gmail.com>
2025-02-05 21:20:48 +0700

    no 2
    Signed-off-by: cotucotam <trancotam@gmail.com>

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<?php
-    function myFirstFunction(){
-        echo "<p> Hello My Name Tam </p>";
+<?php 
+    $name = array('tran', 'co', 'tam');
+    $count = 0;
+    while($count < count($name)){
+        echo "<li>Hello $name[$count]</li>";
+        $count ++;
     }
-    function greet($name){
-        echo "<p> $name is great</p>";
-    }
-    myFirstFunction();
-    myFirstFunction();
-    myFirstFunction();
-    greet('tam');
-    greet('co','tam');
 ?>
-<h1><?php greet('tam'); ?></h1>
-<h2><?php bloginfo('name')?></h2>
-<p><?php bloginfo('description')?></p>
